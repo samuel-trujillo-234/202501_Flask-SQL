388a95ec7042a433d0e91c69606132079ec75c61
Samuel Trujillo <sam@Samuels-MacBook-Pro.local>
2025-04-02 09:44:25 -0500

    Actualizando repositorio con la versión de mi PC

--- Canciones/app/controllers/canciones.py
+++
@@ -1,5 +0,0 @@
-from flask import app
-from flask import render_template, request, redirect, flash
-from flask.app.models.cancion import Cancion
-from flask.app.models.artista import Artista
-
--- Canciones/server.py
+++
@@ -1,9 +0,0 @@
-from flask_app import app
-
-from flask_app.controllers import canciones
-from flask_app.controllers import favoritos
-
-
-if __name__ == "__main__":
-    
-    app.run(debug=True)
